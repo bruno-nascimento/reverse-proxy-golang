8e3906276f434c3476f9935242b231d289acf47c
F1692474 Bruno Caldeira do Nascimento <bruno.nascimento@bb.com.br>
2019-11-05 14:54:43 -0300

    fasthttp is faster

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-proxy
 
 go 1.13
+
+require github.com/valyala/fasthttp v1.6.0
--- main.go
+++ main.go
@@ -1,50 +1,94 @@
 package main
 
+//import (
+//	"fmt"
+//	"net"
+//	"net/http"
+//	"net/http/httputil"
+//	"net/url"
+//	"time"
+//)
+//
+//func main() {
+//
+//	var defaultTransport http.RoundTripper = &http.Transport{
+//		Proxy: nil,
+//		DialContext: (&net.Dialer{
+//			Timeout:   10 * time.Second,
+//			KeepAlive: 30 * time.Second,
+//		}).DialContext,
+//		MaxIdleConns:           1000000,
+//		MaxIdleConnsPerHost:    1000000,
+//		MaxConnsPerHost:        1000000,
+//		IdleConnTimeout:        90 * time.Second,
+//	}
+//
+//
+//	url, err := url.Parse("http://*")
+//
+//	if err != nil {
+//		println(fmt.Sprintf("Error parsing url : %#v", err))
+//	}
+//	proxy := httputil.ReverseProxy{
+//		Director: func(req *http.Request) {
+//			req.Host = "*"
+//			req.URL = url
+//		},
+//		Transport:      defaultTransport,
+//		FlushInterval:  -1,
+//	}
+//
+//	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+//		proxy.ServeHTTP(w, r)
+//	})
+//
+//	server := http.Server{
+//		Addr: "0.0.0.0:7777",
+//	}
+//	server.ListenAndServe()
+//
+//}
+
 import (
-	"fmt"
-	"net"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"time"
+	"github.com/valyala/fasthttp"
+	"log"
 )
 
+var proxyClient = &fasthttp.HostClient{
+	Addr: "*",
+	// set other options here if required - most notably timeouts.
+}
+
+func ReverseProxyHandler(ctx *fasthttp.RequestCtx) {
+	req := &ctx.Request
+	resp := &ctx.Response
+	prepareRequest(req)
+	if err := proxyClient.Do(req, resp); err != nil {
+		ctx.Logger().Printf("error when proxying the request: %s", err)
+	}
+	postprocessResponse(resp)
+}
+
+func prepareRequest(req *fasthttp.Request) {
+	// do not proxy "Connection" header.
+	req.Header.Del("Connection")
+	req.SetHost("*")
+	// strip other unneeded headers.
+
+	// alter other request params before sending them to upstream host
+}
+
+func postprocessResponse(resp *fasthttp.Response) {
+	// do not proxy "Connection" header
+	resp.Header.Del("Connection")
+	resp.Header.Set("proxy", "fasthttp")
+	// strip other unneeded headers
+
+	// alter other response data if needed
+}
+
 func main() {
-
-	var defaultTransport http.RoundTripper = &http.Transport{
-		Proxy: nil,
-		DialContext: (&net.Dialer{
-			Timeout:   10 * time.Second,
-			KeepAlive: 30 * time.Second,
-		}).DialContext,
-		MaxIdleConns:           1000000,
-		MaxIdleConnsPerHost:    1000000,
-		MaxConnsPerHost:        1000000,
-		IdleConnTimeout:        90 * time.Second,
+	if err := fasthttp.ListenAndServe(":8080", ReverseProxyHandler); err != nil {
+		log.Fatalf("error in fasthttp server: %s", err)
 	}
-
-
-	url, err := url.Parse("http://*")
-
-	if err != nil {
-		println(fmt.Sprintf("Error parsing url : %#v", err))
-	}
-	proxy := httputil.ReverseProxy{
-		Director: func(req *http.Request) {
-			req.Host = "*"
-			req.URL = url
-		},
-		Transport:      defaultTransport,
-		FlushInterval:  -1,
-	}
-
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		proxy.ServeHTTP(w, r)
-	})
-
-	server := http.Server{
-		Addr: "0.0.0.0:7777",
-	}
-	server.ListenAndServe()
-
 }
